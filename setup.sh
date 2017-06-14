efa6e86c429869fc225abeb08850fc07f1be4787
Luisa <luisa.fabrizi@gmail.com>
2017-06-14 19:32:09 +0000

    Add script for setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#execute this file in your project directory with $ sh ./setup.sh
+
+# back up old rc.local file
+sudo mv /etc/rc.local /etc/backup_rc.local
+# copy the rc.local file with the printer instructions to /etc
+sudo cp rc.local /etc/rc.local
+
+# copy the printer.sh file with the printer instructions to /bin
+sudo cp printer.sh /bin/printer.sh
